56720f36701b0cba01ae5d9aa89adbc7c34c9cc7
AmirhasanGhafori <amirhasanghafori.tabar@gmail.com>
2024-11-10 19:37:54 +0330

    covert functional component to class component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React,{Component} from "react";
 import Cart from "../UI/Cart";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
 
-  return (
-    <Cart className="bg-gray-900 p-3 flex items-center justify-between max-w-2xl rounded-lg gap-2 m-2 mx-auto">
+class ExpenseItem extends Component {
+  constructor(){
+    super();
+  }
+  render(){
+    return <Cart className="bg-gray-900 p-3 flex items-center justify-between max-w-2xl rounded-lg gap-2 m-2 mx-auto">
       <div className="flex gap-8 items-center">
-        <ExpenseDate date={props.date} />
-        <h2 className="text-white">{props.title}</h2>
+        <ExpenseDate date={this.props.date} />
+        <h2 className="text-white">{this.props.title}</h2>
       </div>
-      <button className="p-2 rounded-lg bg-green-500">${props.amount}</button>
+      <button className="p-2 rounded-lg bg-green-500">${this.props.amount}</button>
     </Cart>
-  );
+  }
 }
+
 
 export default ExpenseItem;
