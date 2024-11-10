21232bdeb02b48088a23f8816d4551e63159a6dd
AmirhasanGhafori <amirhasanghafori.tabar@gmail.com>
2024-11-10 19:42:56 +0330

    add class component to  ExpenseFilter

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,31 +1,71 @@
-function ExpenseFilter(props) {
+import { Component } from "react";
 
-    const dropdownChangeHandler = (event)=>{
-        props.onChangeFilter(event.target.value);
-    }
+class ExpenseFilter extends Component {
+  constructor(){
+    super();
+  }
 
-  return (
-    <>
-      <form className="max-w-2xl mx-auto">
-        <label
-          for="countries"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Select an option
-        </label>
-        <select value={props.selected} onChange={dropdownChangeHandler}
-          id="countries"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        >
-          <option>Choose a country</option>
-          <option value="2024">2024</option>
-          <option value="2023">2023</option>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-        </select>
-      </form>
-    </>
-  );
+  dropdownChangeHandler(event){
+    this.props.onChangeFilter(event.target.value);
+  }
+
+  render(){
+    
+    return (
+      <>
+        <form className="max-w-2xl mx-auto">
+          <label
+            for="countries"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Select an option
+          </label>
+          <select value={this.props.selected} onChange={this.dropdownChangeHandler}
+            id="countries"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option>Choose a country</option>
+            <option value="2024">2024</option>
+            <option value="2023">2023</option>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+            <option value="2020">2020</option>
+            <option value="2019">2019</option>
+          </select>
+        </form>
+      </>
+    );
+  }
 }
+
+// function ExpenseFilter(props) {
+
+//     const dropdownChangeHandler = (event)=>{
+//         props.onChangeFilter(event.target.value);
+//     }
+
+//   return (
+//     <>
+//       <form className="max-w-2xl mx-auto">
+//         <label
+//           for="countries"
+//           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+//         >
+//           Select an option
+//         </label>
+//         <select value={props.selected} onChange={dropdownChangeHandler}
+//           id="countries"
+//           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+//         >
+//           <option>Choose a country</option>
+//           <option value="2024">2024</option>
+//           <option value="2023">2023</option>
+//           <option value="2022">2022</option>
+//           <option value="2021">2021</option>
+//         </select>
+//       </form>
+//     </>
+//   );
+// }
 
 export default ExpenseFilter;
